4c8d535b3778b2eb4c6076a844bb23668417d9cf
Nikhil_Nama <nikhilnama1111@gmail.com>
2026-05-23 15:10:49 +0530

    Introduce DTOs

---
+++ src/main/java/com/nikhil/group/dto/request/CreateTeamRequest.java
@@ -0,0 +1,13 @@
+package com.nikhil.group.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CreateTeamRequest {
+
+    @NotBlank
+    private String name;
+}
